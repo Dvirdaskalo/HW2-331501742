94ac0643488048c7a4a196de95427fec59fe5c8a
eilai007 <delormeeilai1@gmail.com>
2025-12-28 21:44:56 +0200

    very improved ui

---
+++ Assets/sc1.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+[RequireComponent(typeof(SpriteRenderer))]
+public class sc1 : MonoBehaviour
+{
+   
+
+    
+    
+        void Update()
+        {
+            SpriteRenderer sr = GetComponent<SpriteRenderer>();
+
+            float height = Camera.main.orthographicSize * 2f;
+            float width = 5+height * Screen.width / Screen.height;
+
+            sr.size = new Vector2(width,sr.size.y);
+        }
+    
+}
